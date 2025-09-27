e1cb62dbed10ae859d46d93b858a69ece6919e12
tuglam16 <nguyentunglam1216@gmail.com>
2025-09-27 18:10:07 +0700

    Mongoose db

---
+++ src/models/Product.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    category_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category", // tham chiếu tới Category
+      required: true,
+    }
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
