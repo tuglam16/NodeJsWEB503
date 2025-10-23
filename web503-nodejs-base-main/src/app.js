1ed9b66d0652809002b1307d737a48a83f897745
tuglam16 <nguyentunglam1216@gmail.com>
2025-10-23 16:49:53 +0700

    #

--- web503-nodejs-base-main/src/app.js
+++ web503-nodejs-base-main/src/app.js
@@ -11,8 +11,8 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
 
+app.use("/courses", courseRouter);
+
 app.listen(3000, () => {
   console.log(`Server is running on port http://localhost:3000`);
 });
-
-app.use("/courses", courseRouter);
